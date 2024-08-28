8966d42a4e58c5e9b0bb8fac5136210993bb0d9d
richardsonnoel01 <richardsonnoel01@gmail.com>
2024-08-28 18:56:39 -0400

    method

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        printAreaResults();
+    }
+
+    public static void rectangleArea() {
+        System.out.println("5 x 10");
+    }
+
+    public static void triangleArea() {
+        System.out.println("(3 x 6)/2");
+    }
+
+    public static void printAreaResults() {
+        rectangleArea();
+        triangleArea();
+    }
+}
